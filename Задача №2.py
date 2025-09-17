dd80279bbff0d0c3d9b442f4f7e6dea82327a09a
GUTS-8765 <m85798623@gmail.com>
2025-09-17 11:20:04 +0300

    Add files via upload

---
+++ Задача №2.py
@@ -0,0 +1,4 @@
+salary = float(input("Введите зарплату за месяц: "))
+credit_payment = float(input("Введите сумму платежа по кредиту: "))
+utility_debt = float(input("Введите задолженность за коммунальные услуги: "))
+print(f"Сумма, оставшаяся после всех выплат{'remaining_amount':.2f}:рублей")
