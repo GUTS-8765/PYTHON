a36c21637cdcea6293afa3082177b69509ec5515
GUTS-8765 <m85798623@gmail.com>
2025-09-22 16:38:37 +0300

    Update Задача №2.py

--- Задача №2.py
+++ Задача №2.py
@@ -1,4 +1,5 @@
-salary = float(input("Введите зарплату за месяц: "))
-credit_payment = float(input("Введите сумму платежа по кредиту: "))
-utility_debt = float(input("Введите задолженность за коммунальные услуги: "))
-print(f"Сумма, оставшаяся после всех выплат{'remaining_amount':.2f}:рублей")
+salary = int(input("Введите зарплату за месяц: "))
+credit_payment = int(input("Введите сумму платежа по кредиту: "))
+utility_debt = int(input("Введите задолженность за коммунальные услуги: "))
+sum = salary - credit_payment - utility_debt
+print(sum)
