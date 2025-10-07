6be121c29a3e7af6be481014c478fdbb3125a8e0
GUTS-8765 <m85798623@gmail.com>
2025-10-07 10:10:07 +0300

    Delete CLASSWORK directory

--- CLASSWORK/3.py
+++
@@ -1,7 +0,0 @@
-number = float(input("введите число"))
-if number > 0:
-    print("number is positive")
-elif number < 0:
-    print("number is negative")
-else:
-    print("number is equal to zero")
--- CLASSWORK/4.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("введите число: "))
-num2 = float(input("введите число: "))
-if num1 == num2:
-    print("числа равны")
-if num1 >= num2:
-        print(f"{num2}, {num1}")
-else:
-        print(f"{num1}, {num2}")
