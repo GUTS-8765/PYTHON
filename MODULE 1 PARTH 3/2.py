75ee173d67aa299855511d00f23458713f77a76e
GUTS-8765 <m85798623@gmail.com>
2025-11-17 14:49:50 +0300

    Add files via upload

---
+++ MODULE 1 PARTH 3/2.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter two number: "))
+num_3 = int(input("Enter third number: "))
+num_4 = int(input("Enter fourth number: "))
+result = num_1 * num_2 * num_3 * num_4
+print("сумма введеных чисел:", result)
