d2d5f36b4fb94b94d51ebabfc274218fe4b36a09
GUTS-8765 <m85798623@gmail.com>
2025-11-20 12:36:43 +0300

    Add files via upload

---
+++ MODULE 1 PARTH 3/4.py
@@ -0,0 +1,4 @@
+base = float(input("Введите длину основания треугольника: "))
+height = float(input("Введите высоту треугольника: "))
+area = base * height
+print(f"Площадь треугольника равна: {area}")
